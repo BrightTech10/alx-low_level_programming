f491f82104a9515143ebf735e7d811df43fa6c43
Bright <brightiniabasi@gmail.com>
2022-03-11 19:36:25 +0100

    Print string for conditions on last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf ("Last digit of %i is ", n);
-if ((n % 10) > 5)
+if((n % 10) > 5)
 {
-printf("%i and is greater than 5", (n % 10));
+printf("%i and is greater than 5\n", (n % 10));
 }
-else if ((n % 10) == 0)
+else if((n % 10) == 0)
 {
-printf("%i and is 0", (n % 10));
+printf("%i and is 0\n", (n % 10));
 }
-else if ((n % 10) < 6 && (n % 10) != 0)
+else if((n % 10) < 6 && (n % 10) != 0)
 {
 printf("%i and is less than 6 and not 0\n", (n % 10));
 }
