df47acafb08e5fde3bf5f6c0755f700fdf5597e8
Bright <brightiniabasi@gmail.com>
2022-03-15 15:22:06 +0100

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//#include "main.h"
+/**
+ * main - Entry point
+ * Description: Program to print _putchar and move to next line
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+    char c[9] = "_putchar";
+    int i;
+    for (i = 0; i <= 9; i++)
+    {
+        _putchar(c[i]);
+    }
+
+    putchar('\n');
+    return (0);
+
+}
