86a486124cced9bb1964cac9a4e3005627057e0f
Bright <brightiniabasi@gmail.com>
2022-03-16 21:37:23 +0100

    Compute absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,16 +3,15 @@
 #include <limits.h>
 
 /**
- * _abs(int) - computes the absolute value of an integer
+ * _abs(int x) - computes the absolute value of an integer
  *
  * Description: This function computes the absolute value of an integer
  * main - Entry point
  * Return: Always 0 (Success)
 */
 
-int _abs(int)
+int _abs(int x)
 {
-int x;
 int a;
 a = _abs(x);
 printf("%i\n", a);
