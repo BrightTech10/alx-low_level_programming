ea795898246413962346889c663e4750f8b25809
Bright <brightiniabasi@gmail.com>
2022-03-16 18:41:31 +0100

    Computs absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs(int) - computes the absolute value of an integer
+ *
+ * Description: This function computes the absolute value of an integer
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+int abs()
+{
+int x;
+int abs(x);
+}
