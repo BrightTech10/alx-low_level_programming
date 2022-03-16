0d52983c41b2f8710d4292151e42ec96e9ff4859
Bright <brightiniabasi@gmail.com>
2022-03-16 16:25:35 +0100

    Print alphabets a-z in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,9 @@
 
 /**
  *main - check the code
- *Return: Always 0
+ *Description: Prints alphabets a -z in lowercase followed by a new line
+ *Return: Always 0 (Success)
 */
-
-
 
 void print_alphabet(void) /* Prints alphabets a -z in lowercase */
 {
