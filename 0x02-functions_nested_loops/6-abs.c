576544d8435d2ce93639163688b89356ceaf14b7
Bright <brightiniabasi@gmail.com>
2022-03-17 21:10:43 +0100

    Compute absolute number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include <stdio.h>
-#include <limits.h>
 
 /**
- * _abs(int x) - computes the absolute value of an integer x
- *
+ * _abs - computes the absolute value of an integer x
+ * @c: parameter to test for different cases
  * Description: This function computes the absolute value of an integer x
  * main - Entry point
  * Return: Always 0 (Success)
@@ -12,7 +10,12 @@
 
 int _abs(int c)
 {
-int r;
-r = _abs(c);
-return (r);
+if (c < 0)
+{
+c = (-1) * c;
+printf("%d", c);
+}
+c = abs(c);
+printf("%d", c);
+return (0);
 }
