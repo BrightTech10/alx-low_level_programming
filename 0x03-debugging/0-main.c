91b0965effdf60ba060ea599b826c502b3368db7
Bright <brightiniabasi@gmail.com>
2022-03-18 12:27:05 +0100

    Test for positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * positive_or_negative - tests for positive or negative number
@@ -8,9 +9,15 @@
  * Return: Always 0 (Success)
 */
 
-int positive_or_negative(int i)
-{
+void positive_or_negative(i);
 
+int main(void)
+{
+void positive_or_negative(int i)
+{
+int i;
+
+i = 98;
 if (i > 0)
 {
 printf("%i is positive.", i);
@@ -18,6 +25,7 @@ printf("%i is positive.", i);
 else
 {
 printf("%i is negative.", i);
+}
 }
 return (0);
 }
