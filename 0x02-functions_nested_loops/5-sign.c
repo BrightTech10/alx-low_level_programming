5b04e2d266d0eac2b9d3d3782a9174ce7d1994eb
Bright <brightiniabasi@gmail.com>
2022-03-20 20:31:38 +0100

    Print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,22 @@
 
 int print_sign(int n)
 {
+char a = '+';
+char b = '0';
+char c = '-';
 if (n > 0)
 {
-_putchar(+);
+_putchar(a);
 return (1);
 }
 if (n == 0)
 {
-_putchar(0);
+_putchar(b);
 return (0);
 }
 if (n < 0)
 {
-_putchar(-);
+_putchar(c);
 return (-1);
 }
 return (0);
