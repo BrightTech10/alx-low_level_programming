5cf99b137f6c3509027db0747a1840022e26083b
Bright <brightiniabasi@gmail.com>
2022-03-20 21:41:00 +0100

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int print_last_digit(int)
+{
+int c;
+print_last_digit(c);
+return (0);
+}
