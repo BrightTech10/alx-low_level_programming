9d35b9d7b8d12f4af6779289f61387eea078b146
Bright <brightiniabasi@gmail.com>
2022-03-20 20:11:02 +0100

    Print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,31 @@
 #include "main.h"
-
 /**
- * print_sign - print the sign of a number
+ * print_sign(int n) - print the sign of a number
  * and returns 1 if c is a letter, lowercase,
  * or uppercase, otherwise 0
- * @c: parameter to test for different cases
+ * @n: parameter to test for different cases
  * Description: This function checks for alphabetic characters
  * main - Entry point
  * Return: Always 0 (Success)
 */
 
-int print_sign(int n)
+int int print_sign(int n)
 {
-if(n > 0)
+int n = 0;
+if (n > 0)
 {
-return (1);
 printf("+");
+return (1);
 }
-if(n == 0)
+if (n == 0)
 {
-return (0);
 printf("0");
+return (0);
 }
-if(n < 0)
+if (n < 0)
 {
-return (-1);
 printf("-");
+return (-1);
 }
 return (0);
 }
