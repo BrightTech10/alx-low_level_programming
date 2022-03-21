3d388a6c6b3366c83b0e091ac185329d5b5fe6f5
Bright <brightiniabasi@gmail.com>
2022-03-21 23:52:40 +0100

    Swap integer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @*x: first integer value
+ * @*y: second integer value
+*/
+
+swap_int(int *x, int *y)
+{
+int temp;
+temp = *x;
+*x = *y;
+*y = temp;
+}
