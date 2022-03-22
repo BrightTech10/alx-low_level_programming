b960118b018afe9dd32c8aafb378dd1c5bf5a415
Bright <brightiniabasi@gmail.com>
2022-03-22 02:18:10 +0100

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - function to print a string
+ * @a: array of characters
+ * Return: return the result of the function
+*/
+
+void _puts(char *a)
+{
+char x;
+x = puts(a);
+return (x);
+}
