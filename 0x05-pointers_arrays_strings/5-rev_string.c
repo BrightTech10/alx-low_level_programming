517d2ce82fcc18df318bc8a287a0776ebcf482d2
Bright <brightiniabasi@gmail.com>
2022-03-22 15:51:33 +0100

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,21 +3,23 @@
 
 /**
  * rev_string - function that reverses string
+ * swap - swaps the values of variables
  * @a: pointer variable of type char
+ * @x: variable to be swapped with y
+ * @y: variable to be swapped with x
 */
-void swap(char *a, char *b)
+void swap(char *x, char *y)
 {
-    char temp = *a;
-    *a = *b;
-    *b = temp;
+char temp = *x;
+*x = *y;
+*y = temp;
 }
-
 
 void rev_string(char *a)
 {
 int len, i;
 len = strlen(a);
-for (i = 0; i < len/2; i++)
+for (i = 0; i < len / 2; i++)
 {
 swap(&a[i], &a[len - i - 1]);
 }
