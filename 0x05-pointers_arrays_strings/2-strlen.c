c94a993bf1ec296cf05337ac9a9e75c54454d9e2
Bright <brightiniabasi@gmail.com>
2022-03-22 01:33:40 +0100

    Find length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,7 +6,14 @@
  * Return: return the length of string
 */
 
+
+
 int _strlen(char *a)
 {
-int length = _strlen(a);
+	char *str;
+	int len;
+	str = "Holberton!";
+	len = strlen(str);
+	printf("%d\n", len);
+	return (0);
 }
