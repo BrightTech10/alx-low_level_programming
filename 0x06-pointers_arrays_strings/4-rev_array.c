003e9aaedce461498a428816557493e9eb23c482
Bright <brightiniabasi@gmail.com>
2022-03-23 23:45:39 +0100

    Reverse elements of an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - to print an array;
+ * add ", " when not on the zeroth index
+ *
+ * @a: pointer variable to array
+ * @n: size of array
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+
+i = 0;
+while (i < n)
+{
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i++;
+}
+printf("\n");
+}
+
+/**
+ * reverse_array - to reverse the content of an array
+ *
+ * @a: pointer variable to array
+ * @n: size of array
+ *
+ * Return: returns reversed array elements
+*/
+
+void reverse_array(int *a, int n)
+{
+int i, result;
+for (i = 0; i < n / 2; i++)
+{
+result = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = result;
+}
+return (result);
+}
