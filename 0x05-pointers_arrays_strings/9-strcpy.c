22beb7c750dd6d8362fac21ee5db09acf2472083
Bright <brightiniabasi@gmail.com>
2022-03-23 05:49:13 +0100

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_strcpy - function that copies a string
+ *
+ * @dest: first string
+ * @dest: second string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+}
