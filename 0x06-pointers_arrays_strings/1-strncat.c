c1369fe8356866d60234cacf27c38730fb790717
Bright <brightiniabasi@gmail.com>
2022-03-23 15:14:13 +0100

    Concatenate n bytes to str2 to str1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings using at most n bytes
+ *
+ * @dest: destination string
+ * @src: source string; to be appended to the destination string
+ * @n: n bytes from source string
+ *
+ * Return: returns pointer to destination string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+strncat(dest, src, n);
+}
