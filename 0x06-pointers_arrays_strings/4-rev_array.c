8319e8ff2226dfb177c19dba77f85f8527e34c2f
Bright <brightiniabasi@gmail.com>
2022-03-25 14:15:42 +0100

    Reverse elements of an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * print_array - prints array elements with comma and space
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+i = 0;
+while (i < n)
+{
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i++;
+}
+printf("\n");
+}
 
 /**
  * reverse_array - to reverse the elements of an array
