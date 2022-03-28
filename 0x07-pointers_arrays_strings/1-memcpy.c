b66a1e0f3d66d7ee3b1c6b82a107fbd916695deb
Bright <brightiniabasi@gmail.com>
2022-03-28 14:07:21 +0100

    Copies n bytes of memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes from src to be copied
+ * Return: returns pointer to destination memory area
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+
+return (dest);
+}
+}
