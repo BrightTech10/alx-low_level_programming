c76fa20aa19492d1901f20a22d74dcd3f9ce78af
Bright <brightiniabasi@gmail.com>
2022-03-30 17:08:23 +0100

    Compute the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function to multiply x y times
+ * @x: integer to be multiplies
+ * @y: power
+ * Return: returns -1 if y < 0; 1 if y == 0
+*/
+
+
+int _pow_recursion(int x, int y)
+{
+int i;
+i = 0;
+
+if (y == 0)
+{
+return (1);
+}
+
+if (y < 0)
+{
+return (-1);
+}
+
+return (x * _pow_recursion(x, y - 1));
+}
