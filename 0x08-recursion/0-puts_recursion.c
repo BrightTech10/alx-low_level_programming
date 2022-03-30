90a9f0ab230b2552a8bd809f5ce5335471234c49
Bright <brightiniabasi@gmail.com>
2022-03-30 15:31:40 +0100

    Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print a string
+ * followed by a new line
+ *
+ * @s: pointer to string
+*/
+
+void _puts_recursion(char *s)
+{
+puts (s);
+}
