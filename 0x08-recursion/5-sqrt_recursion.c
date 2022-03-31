48f573a4524a53ff1061313ed03c1639bbb3bca0
Bright <brightiniabasi@gmail.com>
2022-03-31 15:20:03 +0100

    Compute square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function that returns the natural square root of a number
- * @n: number to be checked
- * Return: returns i if n equals 1 or -1 if n is less than 1.
- * It also calls the getRoot function
-*/
-
-int _sqrt_recursion(int n)
-{
-if (n == 1)
-return (1);
-
-else if (n < 1)
-return (-1);
-
-else
-return getRoot(n, 2);
-}
-
-/**
  * getRoot - function that computes the square root of a number
  * @k: number to be tested
  * @i: number used to check for root
@@ -36,4 +17,24 @@ else if ((i * i) == k)
 return (i);
 }
 return (getRoot(k, i + 1));
+}
+
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: number to be checked
+ * Return: returns i if n equals 1 or -1 if n is less than 1.
+ * It also calls the getRoot function
+*/
+
+int _sqrt_recursion(int n)
+{
+if (n == 1)
+return (1);
+
+else if (n < 1)
+return (-1);
+
+else
+return getRoot(n, 2);
 }
