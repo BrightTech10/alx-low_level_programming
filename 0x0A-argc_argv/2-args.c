9a17b483ec8b8ad8bc113d5574f4b789fb8b089b
Bright <brightiniabasi@gmail.com>
2022-04-02 21:55:04 +0100

    Print all arguments received

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * main - function to print name of program
+ * main - function to print all arguments received
  * @argc: argument count
  * @argv: argument vector
  * Return: returns 0 (Success)
@@ -11,8 +11,9 @@
 
 int main(int argc, char *argv[])
 {
+printf("%s\n", argv[0]);
 int i;
-for (i = 0; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
