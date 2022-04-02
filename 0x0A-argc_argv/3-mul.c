24789e8f29ed3369510dcc1ec800d5e796111fd8
Bright <brightiniabasi@gmail.com>
2022-04-03 00:21:49 +0100

    Multiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,26 +3,32 @@
 #include <string.h>
 
 /**
- * main - function to multiply two numbers
+ * main - function to print name of program
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: returns 0 (Success)
+ * Return: returns 1 if program does not receive two argument; 0 is successful
  */
 
 int main(int argc, char *argv[])
 {
-/* If program receives less than two or no argument, print error */
-if (argc < 3)
+if (argc == 1)
 {
 printf("Error\n");
 return (1);
 }
 
-int mul; /* Declare a int variable to store the result of multiplication */
-mul = strtol(argv[1], NULL, 10) * strtol(argv[2], NULL, 10);
-
+if (argc > 1)
+{
+int i;
+int mul;
+mul = 1;
+for (i = 1; i < argc; i++)
+{
+mul = mul *strtol(argv[i], NULL, 10);
+}
 printf("%d\n", mul);
+}
 
 return (0);
 }
