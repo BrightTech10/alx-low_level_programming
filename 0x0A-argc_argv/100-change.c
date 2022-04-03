781b718222a98c5494e340a8bed0ac0aa4ff2bc0
Bright <brightiniabasi@gmail.com>
2022-04-03 19:14:56 +0100

    Print minimum number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - function to print minimum number of coins
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if (argc < 0)
+{
+printf("0\n");
+return (0);
+}
+int i, cents, total_cents;
+total_cents = 0;
+cents = atoi(argv[1]);
+while (cents > 0)
+{
+if (cents >= 25)
+cents -= 25;
+else if (cents >= 10)
+cents -= 10;
+else if (cents >= 5)
+cents -= 5;
+else if (cents >= 2)
+cents -= 2;
+else if (cents >= 1)
+cents -= 1;
+total_cents += 1;
+}
+printf("%d\n", total_cents);
+return (0);
+}
