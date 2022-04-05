ae3227bce3c8a41782a24586b55d05ff9d1e7923
Bright <brightiniabasi@gmail.com>
2022-04-05 12:28:39 +0100

    Create and initialize array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array; initialize to specific character
+ * @size: size of array
+ * @c: specific character
+ * Return: returns NULL if size = 0, else returns pointer to allocated memory (str)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+  int count; /* Declare variable for loop count */
+  char *str; /* Declare pointer to allocated memory */
+
+  /* Allocate memory and store address in pointer */
+  str = malloc(sizeof(char) * size);
+
+  /* For loop to initialize all elements of the array with char c */
+  for (count = 0; count < size; count++)
+  {
+     str[count] = c;
+  }
+
+  /* If size of array is 0, return NULL */
+  if (size == 0)
+    return (0);
+
+  /* Else return pointer to allocated memory */
+  else
+    return (str);
+}
