bb6bb73ea30fd4fde82eb6ab762cb39781889cf4
Bright <brightiniabasi@gmail.com>
2022-04-08 00:39:37 +0100

    Return pointer to memory of new string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,36 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 /**
  * _strdup - function to return pointer to newly allocated memory
  * which contains a copy of the string given as a parameter
  * @str: string passed as parameter
- * Return: returns NULL if size = NULL or pointer fails
+ * Return: returns NULL if size = 0 or pointer fails
  * else returns pointer to newly allocated memory
  */
+
 char *_strdup(char *str)
 {
-int size; /* Declare int variable size */
-/* Initialize size to the size of string str */
-size = sizeof(str);
-char *ptr; /* Declare pointer variable */
-char cpy[size]; /* Declare variable to hold copy of string */
+char *ptr;
+ptr = malloc(sizeof(str));
+strcpy(ptr, str);
 
-/* Copy string str to cpy */
-strcpy(cpy, str);
-
-/* Allocate memory and store address in pointer */
-ptr = malloc(sizeof(char) * sizeof(cpy));
-
-/* If pointer fails, return NULL*/
-/* This is to avoid segmentation fault */
-if (!str)
-{
-printf("Error! Could not allocate space for array\n");
-return (NULL);
-}
-/* If size of array is NULL, return NULL */
-if (size == NULL)
+if (str == NULL)
 return (NULL);
 
 return (ptr);
