20f0862bc16ad2f9b87a9e743fe7860bc385e447
Bright <brightiniabasi@gmail.com>
2022-04-09 22:41:50 +0100

    Print sum of multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 
 /**
- * main - compute sum of multiples of an integer and print
+ * sum_mul - compute sum of multiples of an integer
+ * @n: integer variable to be computed
+ * Return: return total value
+ */
+
+int sum_mul(int n)
+{
+int sum1, sum2, sum3, total, i;
+sum1 = 0;
+sum2 = 0;
+sum3 = 0;
+for (i = 0; i < n; i++)
+{
+if ((i % 3) == 0)
+{
+sum1 += i;
+}
+if ((i % 5) == 0)
+{
+sum2 += i;
+}
+if ((i % 15) == 0)
+{
+sum3 += i;
+}
+}
+total = sum1 + sum2 - sum3;
+return (total);
+}
+
+/**
+ * main -  prints sum of multiples
  * Return: return 0 (Success)
  */
 
 int main(void)
 {
-long int sum1, sum2, sum3, final_sum, i;
-sum1 = 0;
-sum2 = 0;
+int final_sum;
 
-for (i = 0; i < 1024; i++)
-{
-
-if ((i % 3) == 0)
-{
-sum1 += i;
-}
-
-if ((i % 5) == 0)
-{
-sum2 += i;
-}
-
-if ((i % 15) == 0)
-{
-sum3 += i;
-}
-
-}
-final_sum = sum1 + sum2 - sum3;
-printf("%ld\n", final_sum);
+final_sum = sum_mul(1024);
+printf("%d\n", final_sum);
 
 return (0);
 }
