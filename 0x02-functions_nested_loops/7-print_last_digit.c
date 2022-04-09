3d9e8d7cef76022bdcd9c1541fcc765fbcdc7176
Bright <brightiniabasi@gmail.com>
2022-04-09 09:03:41 +0100

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@
 
 int print_last_digit(int c)
 {
-char last_digit;
+int last_digit;
 if (c < 0)
 {
-c = c * -1;
+c = -c;
 }
 last_digit = c % 10;
-_putchar(last_digit);
+_putchar(last_digit + '0');
 return (last_digit);
 }
