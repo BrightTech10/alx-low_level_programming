7a11247d39563f674010df1221499fe4021988f6
Bright <brightiniabasi@gmail.com>
2022-04-11 16:35:29 +0100

    Print file compilation path

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print name of file from where program is compiled
+ * Return: return 0 (Success)
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
+
