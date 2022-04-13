2edba130bc0ca9c109bb6dcc84a5437563e40644
Bright <brightiniabasi@gmail.com>
2022-04-13 04:18:59 +0100

    Initialize a variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,17 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+/* If value of my_dog is NULL, print NULL*/
+if (d == NULL)
+{
+printf("");
+}
+
+else
+{
 d->name = name; /* Initialize name of dog */
 d->age = age; /* Initialize age of dog */
 d->owner = owner; /* Initialize owner of dog */
+}
+
 }
