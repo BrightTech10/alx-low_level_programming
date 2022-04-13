9159a7207723b17864c14f27dbf6825a5f287512
Bright <brightiniabasi@gmail.com>
2022-04-13 20:26:00 +0100

    Execute function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,20 +6,22 @@
  * given as a parameter
  * @array: pointer to array
  * @size: size of array
- * @action: function pointer
+ * @action: pointer to a function
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (array && size && action)
+if (array == NULL || size <= 0 || action == NULL)
 {
+return;
+}
 
+else
+{
 int i; /* Declare i for loop */
 for (i = 0; i < size; i++)
 {
 action(array[i]);
 }
-
 }
-
 }
