1ae78634ee4c722f9e7d7714e8af4a748a2912da
Bright <brightiniabasi@gmail.com>
2022-04-13 20:15:29 +0100

    Execute function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,15 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+if (array && size && action)
+{
 
 int i; /* Declare i for loop */
 for (i = 0; i < size; i++)
 {
 action(array[i]);
+}
+
 }
 
 }
