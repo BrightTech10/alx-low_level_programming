db34ca81fd1a9fe4efbfaff79fc15016dafa9b80
Bright <brightiniabasi@gmail.com>
2022-04-14 21:33:05 +0100

    Compute sum of arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - variadic function to compure sum of arguments
+ * @n: number of arguments
+ *
+ * Return: returns 0 if n is 0, else sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+int i, sum;
+
+va_start(args, n);
+
+if (n == 0)
+return (0);
+
+sum = 0;
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, const unsigned int);
+}
+va_end(args);
+return (sum);
+}
