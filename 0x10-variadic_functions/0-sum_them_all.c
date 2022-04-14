279c2b906a5daed40de7f262319d6d00d88538d5
Bright <brightiniabasi@gmail.com>
2022-04-14 22:05:53 +0100

    Compute sum of arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,23 +11,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-va_list args;
-int i, sum;
-sum = 0;
-
-if (n == 0)
-return (0);
+va_list args; /* Variable to iterate arguments */
+int i, sum; /* Declare i and sum; i for looping */
+sum = 0; /* Initialize sum to 0 */
 
 va_start(args, n);
 
-if (n > 0)
-{
 for (i = 0; i < n; i++)
 {
 sum += va_arg(args, const unsigned int);
 }
-}
-
 va_end(args);
 return (sum);
 }
