23ccf08e2d3dd56f05baf26ce26dacdca78e7190
Bright <brightiniabasi@gmail.com>
2022-04-21 18:02:29 +0100

    Print all list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,19 +13,21 @@ size_t print_list(const list_t *h)
 {
 size_t count; /* Declare count for number of nodes */
 
-count = 0;
+count = 0; /* initialize count to 0 */
+
+/* Loop through list while list is not empty */
 while (h != NULL)
 {
-if (h->str == NULL)
+if (h->str == NULL) /* condition for empty string */
 {
 printf("[0] (nil)\n");
 }
-else
+else /* if string != empty, print list elements */
 printf("[%u] %s\n", (h->len), (h->str));
 
 h = h->next;
 count++;
 }
-
+/* return number of nodes */
 return (count);
 }
