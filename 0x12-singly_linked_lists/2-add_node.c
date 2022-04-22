0b65b7479d7717258bd40eedf271c7bdd87db281
Bright <brightiniabasi@gmail.com>
2022-04-22 03:16:07 +0100

    Add node at beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,18 +9,16 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-/* Declare variable to get length of str */
-unsigned int len;
+list_t *newNode;/* Create new node to store new values; */
+unsigned int len;/* Declare variable to get length of str */
 len = 0;
 while (str[len] != '\0')
 {
 len++;
 }
 
-/* Create new node to store new values; */
 /* and allocate memory to new node */
 /* Return NULL is if memory allocation fails */
-list_t *newNode;
 newNode = malloc(sizeof(list_t));
 if (newNode == NULL)
 return (NULL);
@@ -35,4 +33,5 @@ newNode->next = *head;
 /* head now points to newNode */
 *head = newNode;
 
+return (*head);
 }
