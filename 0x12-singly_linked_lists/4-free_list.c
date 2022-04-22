eecab87dd1033a022e4a838ac4f86cff7fc317ba
Bright <brightiniabasi@gmail.com>
2022-04-22 05:04:20 +0100

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,17 +7,22 @@
 
 void free_list(list_t *head)
 {
-/*declare variables to be used*/
+/* declare variables to be used */
 list_t *newNode, *oldNode;
-/*set both pointers to address of head*/
+
+/* set both pointers to address of head */
 newNode = head;
 oldNode = head;
-/*traverse the nodes and free the spaces*/
+
+/* traverse the nodes and free the spaces */
 while (newNode != NULL)
 {
-newNode = newNode->next;/*ptr is now at the next address*/
-free(oldNode->str);/*old frees the address at previous node*/
+/* ptr is now at the next address */
+newNode = newNode->next;
+/* old frees the address at previous node */
+free(oldNode->str);
 free(oldNode);
-oldNode = newNode;/*old is set to next address*/
+/* old is set to next address */
+oldNode = newNode;
 }
 }
