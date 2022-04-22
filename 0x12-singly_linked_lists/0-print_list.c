2edbcd238f2ca419c3c88e9cd7d470d9d9b7a75b
Bright <brightiniabasi@gmail.com>
2022-04-22 02:07:55 +0100

    Print all list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,13 +3,15 @@
 /**
  * print_list - prints all elements in a list
  * @h: pointer to list
- * Return: number of elements printed
+ * Return: returns number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-/*declare variables to be used in loop and node*/
-size_t count = 0;
+/* Declare count for number of nodes */
+size_t count;
+
+count = 0;
 /* Loop through list while list is not empty */
 while (h != NULL)
 {
@@ -18,10 +20,11 @@ if (h->str == NULL)
 printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
-/* Point to address of the next struct*/
+
+/* move to next node */
 h = h->next;
 count++;
 }
-
+/* return number of nodes */
 return (count);
 }
