52ab64d7fc067584062154d69d3c306dfcc0abc0
Bright <brightiniabasi@gmail.com>
2022-04-25 20:51:48 +0100

    Add new node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 listint_t *newNode;
 newNode = malloc(sizeof(listint_t));
 if (newNode == NULL)
+{
+free(newNode);
 return (NULL);
+}
 
 newNode->n = n;
 newNode->next = *head;
