bd712615f8cc5acafe9d55a5f4c3fffd80bb9ca3
Bright <brightiniabasi@gmail.com>
2022-04-25 20:50:42 +0100

    Add new node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,14 +8,14 @@
  *
  * Description: the new node is made to point to previous
  * nodes while head node points to the new node
- * Return: returns address to new node
+ * Return: returns address of new node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *newNode;
 newNode = malloc(sizeof(listint_t));
 if (newNode == NULL)
-return (1);
+return (NULL);
 
 newNode->n = n;
 newNode->next = *head;
