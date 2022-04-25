74f599c72ffb859f39e4351136ab8269acc6c9ea
Bright <brightiniabasi@gmail.com>
2022-04-25 21:49:16 +0100

    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,29 +1,21 @@
 #include "lists.h"
 
 /**
- * free_list - free all the malloce'ed blocks
+ * free_list - frees list
  * @head: pointer to the first node
  */
 
 void free_listint(listint_t *head)
 {
-/* declare variables to be used */
-listint_t *newNode, *oldNode;
+listint_t *temp;
+temp = *head;
 
-/* set both pointers to address of head */
-newNode = head;
-oldNode = head;
-
-/* traverse the nodes and free the spaces */
-while (newNode != NULL)
+while (temp != NULL)
 {
-/* ptr is now at the next address */
-newNode = newNode->next;
-/* old frees the address at previous node */
-free(oldNode->n);
-free(oldNode);
-/* old is set to next address */
-oldNode = newNode;
+temp = temp->next;
+
+free(head);
+head = temp;
 }
 }
 
