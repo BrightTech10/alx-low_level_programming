9fc1f7e17063f5c50d36f00fe5ecb288dee797d0
Bright <brightiniabasi@gmail.com>
2022-04-29 21:48:20 +0100

    Return bit at given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "limits.h"
 
 /**
  * get_bit - returns value of a bit at an index
@@ -10,18 +11,19 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned int i, j;
-unsigned long int array[16];
+int size = (sizeof(n) * 8);
+int binary[size];
 
 for (i = 0; n != 0; i++)
 {
-array[i] = n % 2;
+binary[i] = n % 2;
 n = n / 2;
 }
 
 for (j = i - 1; j >= 0; j--)
 {
 if (j == index)
-return (array[j]);
+return (binary[j]);
 
 else if (index < 0)
 return (-1);
