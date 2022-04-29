c40b83f01eac5ee3f39fb52b7d3644bd6c3cd29a
Bright <brightiniabasi@gmail.com>
2022-04-29 13:31:38 +0100

    Convert to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,15 @@ unsigned int binary_to_uint(const char *b)
 unsigned int sum, len, decVal;
 int i;
 
-len = strlen(b);
-decVal = 1;
-sum = 0;
-
-if (!b)
+if (b == NULL)
 return (0);
 
 else
 {
+len = strlen(b);
+decVal = 1;
+sum = 0;
+
 for (i = (len - 1); i >= 0; i--)
 {
 if (b[i] != '0' && b[i] != '1')
@@ -30,7 +30,6 @@ sum += decVal;
 
 decVal *= 2;
 }
-}
-
 return (sum);
+}
 }
