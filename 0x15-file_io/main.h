8da3eb6cd91ffa984eec733ee2ee083b53922f81
Bright <brightiniabasi@gmail.com>
2022-05-04 10:50:45 -0700

    Add: Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,8 +2,14 @@
 #define _MAIN_H_
 
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+/* Prototypes */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
