07f74d6b9dc70a8dd993f1dec07a46d0f6862bf3
Bright <brightiniabasi@gmail.com>
2022-05-04 13:32:34 -0700

    Create new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates new file
+ * @filename: name of file to be created
+ * @text_content: content to be written to the file
+ *
+ * Return 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int  length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* Create new file if it doesn't exist */
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (0);
+	
+	/* To get length of text_content */
+	while(text_content[length] != '\0')
+		length++;
+	
+	/* Write text_content to the file pointed to by fd */
+	write(fd, text_content, length);
+	
+	close(fd);
+
+	return (1);
+}
+
+
