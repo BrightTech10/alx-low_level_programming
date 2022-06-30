9bd425c18890084ade6877aa39f11c01fb7f97aa
Bright <brightiniabasi@gmail.com>
2022-06-30 19:11:21 +0100

    Add: Task 1-2 :tada:, Update Task 0 :bug:

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - computes and returns the index of a key
+ *
+ * Description: the function implements the djb2 algorithm on @key
+ * to generate a hash value. This value is computed with @size to
+ * generate key index.
+ *
+ * @key: key
+ * @size: size of key
+ * Return: returns key index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
