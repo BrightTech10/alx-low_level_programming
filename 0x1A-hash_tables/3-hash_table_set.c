42e0934629301872a4313cf81d8ccf4a923d8874
Bright <brightiniabasi@gmail.com>
2022-07-01 10:55:52 +0100

    Updated :bug:

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *slot_entry, *prev;
 
-	if (ht == NULL || key == '\0' || value == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
 
 	/* get the index */
